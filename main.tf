fa7240b587600c7fb99331a6b5bdd9a754d3ce84
jpapazian2000 <jpapazian@hashicorp.com>
2021-04-14 14:53:31 +0200

    changed syntax

--- main.tf
+++ main.tf
@@ -135,10 +135,10 @@ resource "vsphere_virtual_machine" "LinuxVM-withDataDisk" {
     }
   }
   extra_config = {
-    "guestinfo.metadata"           = base64encode(file("${path.module}/templates/metadata.yaml"))
-    "guestinfor.metadata.encoding" = "base64"
-    "guestinfo.userdata"           = base64encode(file("${path.module}/templates/userdata.yaml"))
-    "guestinfo.userdata.encoding"  = "base64"
+    "guestinfo.metadata"          = base64encode(file("${path.module}/templates/metadata.yaml"))
+    "guestinfo.metadata.encoding" = "base64"
+    "guestinfo.userdata"          = base64encode(file("${path.module}/templates/userdata.yaml"))
+    "guestinfo.userdata.encoding" = "base64"
   }
   tags = [vsphere_tag.tag.id]
   
