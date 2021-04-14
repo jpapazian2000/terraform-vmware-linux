86b91bd52891666898eedf50da4caa2181723a97
jpapazian2000 <jpapazian@hashicorp.com>
2021-04-14 17:10:04 +0200

    removed custextra config

--- main.tf
+++ main.tf
@@ -134,12 +134,12 @@ resource "vsphere_virtual_machine" "LinuxVM-withDataDisk" {
       ipv4_gateway    = var.vmgateway
     }
   }
-  extra_config = {
-    "guestinfo.metadata"          = base64encode(file("${path.module}/templates/metadata.yaml"))
-    "guestinfo.metadata.encoding" = "base64"
-    "guestinfo.userdata"          = base64encode(file("${path.module}/templates/userdata.yaml"))
-    "guestinfo.userdata.encoding" = "base64"
-  }
+  #extra_config = {
+  #  "guestinfo.metadata"          = base64encode(file("${path.module}/templates/metadata.yaml"))
+  #  "guestinfo.metadata.encoding" = "base64"
+  #  "guestinfo.userdata"          = base64encode(file("${path.module}/templates/userdata.yaml"))
+  #  "guestinfo.userdata.encoding" = "base64"
+  #}
   tags = [vsphere_tag.tag.id]
   
 }
